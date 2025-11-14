7fed02f29c7cf74a6145bbbc117b65153b2fb2ed
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-11-13 22:27:45 -0300

    chore: atualiza docker

--- frontend/components/layout/app-shell.tsx
+++ frontend/components/layout/app-shell.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
-import { cn } from "@/lib/utils";
 
 const links = [
   { href: "/agendamentos", label: "Agendamentos" },
